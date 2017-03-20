4e7f8aeb09758872de08b562820608eba53cd743
Alex Hu <alexhubnds@icloud.com>
2017-03-20 16:02:39 -0400

    init

---
+++ CourseDataUpdate/FirebaseUpdateService.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  CourseDataUpdate
+//
+//  Created by Yuanze Hu on 3/20/17.
+//  Copyright © 2017 Yuanze Hu. All rights reserved.
+//
+
+import Firebase
+
+
+class FirebaseUpdateService{
+    init(){
+        
+        
+    }
+    
+    //this function will be called to put a new course on firebase
+    func postCourse(term:String,courseID:String, name:String,description: String, block:String, time:String,bookUrl:String ,teacherUrl:String)->Bool{
+        return false
+    }
+    
+
+    
+    
+    
+    
+    
+    
+}
---
+++ CourseDataUpdate/LoadingCourseInfo.swift
@@ -0,0 +1,23 @@
+//
+//  LoadingCourseInfo.swift
+//  CourseDataUpdate
+//
+//  Created by Yuanze Hu on 3/20/17.
+//  Copyright © 2017 Yuanze Hu. All rights reserved.
+//
+
+import Foundation
+
+
+class LoadingCourseInfo {
+    
+    
+    init(){
+        
+    }
+    
+    
+    
+    
+    
+}
